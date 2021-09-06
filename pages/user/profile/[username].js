9fc6c3254bd1783e4a38664032a16d6466e65448
pellul <paulinhoellul@gmail.com>
2021-09-06 12:48:31 +0300

    changed profile page

--- pages/user/profile/[username].js
+++ pages/user/profile/[username].js
@@ -9,20 +9,35 @@ import {
   TableRow,
   Button,
   TableCell,
-} from '@material-ui/core';
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import axios from 'axios';
+} from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
+import { makeStyles } from "@material-ui/styles";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+  },
+  // heading: {
+  //   fontSize: theme.typography.pxToRem(15),
+  //   fontWeight: theme.typography.fontWeightRegular,
+  // },
+}));
 export async function getServerSideProps(context) {
   return { props: { username: context.params.username } };
 }
 export default function profile({ username }) {
+  const classes = useStyles();
   const router = useRouter();
   const [result, setResult] = useState(null);
   useEffect(async () => {
     try {
-      const resp = await axios.post('/api/getUserStatistics', { username });
+      const resp = await axios.post("/api/getUserStatistics", { username });
       console.log(resp.data);
       setResult(resp.data);
     } catch (err) {
@@ -34,40 +49,61 @@ export default function profile({ username }) {
     <Container>
       <Card>
         <CardContent>
-          <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <Avatar style={{ width: '20%', height: '20%' }} />
+          <Box
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar style={{ width: "20%", height: "20%" }} />
             <br />
             <Typography variant="h3">
-              {`${result?.firstName ?? 'john'} ${result?.lastName ?? 'doe'}`}
+              {`${result?.firstName ?? "john"} ${result?.lastName ?? "doe"}`}
             </Typography>
             <br />
             <br />
             <br />
-            <Typography variant="h4" style={{ display: 'flex', alignSelf: 'flex-start' }}>
+            <Typography
+              variant="h4"
+              style={{ display: "flex", alignSelf: "flex-start" }}
+            >
               Info
             </Typography>
             <Table>
               <TableRow>
                 <TableCell>Email</TableCell>
-                <TableCell>{result?.email ?? 'someone@example.com'}</TableCell>
+                <TableCell>{result?.email ?? "someone@example.com"}</TableCell>
                 <TableCell>
                   <Button variant="contained">Change</Button>
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell style={{ borderBottom: 'none' }}>Username</TableCell>
-                <TableCell style={{ borderBottom: 'none' }}>
-                  {result?.username ?? 'johndoe'}
+                <TableCell style={{ borderBottom: "none" }}>Username</TableCell>
+                <TableCell style={{ borderBottom: "none" }}>
+                  {result?.username ?? "johndoe"}
                 </TableCell>
-                <TableCell style={{ borderBottom: 'none' }}>
-                  <Button onClick={() => router.push('/user/updateUsername')} variant="contained">
+                <TableCell style={{ borderBottom: "none" }}>
+                  <Button
+                    onClick={() => router.push("/user/updateUsername")}
+                    variant="contained"
+                  >
                     Change
                   </Button>
                 </TableCell>
               </TableRow>
             </Table>
-            <Box style={{ display: 'flex', justifyItems: 'center', flexDirection: 'column' }}>
-              <Button onClick={() => router.push('/user/updatePassword')} variant="contained">
+            <Box
+              style={{
+                display: "flex",
+                justifyItems: "center",
+                flexDirection: "column",
+              }}
+            >
+              <Button
+                onClick={() => router.push("/user/updatePassword")}
+                variant="contained"
+              >
                 Change Password
               </Button>
             </Box>
@@ -76,21 +112,46 @@ export default function profile({ username }) {
           <br />
           <br />
           <br />
-          <Typography variant="h4" style={{ display: 'flex', alignSelf: 'flex-start' }}>
-            Statistics
-          </Typography>
-          <Table>
-            <TableRow>
-              <TableCell>Last Upload: </TableCell>
-              <TableCell>{result?.lastUploadDate ?? '-'}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell style={{ borderBottom: 'none' }}>Total HARs Uploaded: </TableCell>
-              <TableCell style={{ borderBottom: 'none' }}>{result?.totalUploads ?? '0'}</TableCell>
-            </TableRow>
-          </Table>
+
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography
+                variant="h5"
+                style={{ display: "flex", alignSelf: "flex-start" }}
+              >
+                Statistics
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Table>
+                <TableRow>
+                  <TableCell>Last Upload: </TableCell>
+                  <TableCell>{result?.lastUploadDate ?? "-"}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell style={{ borderBottom: "none" }}>
+                    Total HARs Uploaded:{" "}
+                  </TableCell>
+                  <TableCell style={{ borderBottom: "none" }}>
+                    {result?.totalUploads ?? "0"}
+                  </TableCell>
+                </TableRow>
+              </Table>
+            </AccordionDetails>
+          </Accordion>
         </CardContent>
       </Card>
+      <Button
+        onClick={() => router.push(`/user/${username}/heatmap`)}
+        fullWidth
+        variant="contained"
+      >
+        Visualise Your Data
+      </Button>
     </Container>
   );
 }
